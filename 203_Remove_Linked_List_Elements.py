959883599b4d60ced9947ecdc3dda7aef8a527f7
mahmoudnabil133 <hodanabil144@gmail.com>
2023-12-25 10:52:03 +0200

    remove element with val value

--- 203_Remove_Linked_List_Elements.py
+++ 203_Remove_Linked_List_Elements.py
@@ -1,1 +1,26 @@
-203. Remove Linked List Elements
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
+        while head:
+            if head.val == val:
+                head = head.next
+            else:
+                break
+        if not head:
+            return None
+        
+        itr = head
+
+        while itr.next:
+            if itr.next.val == val:
+                itr.next = itr.next.next
+            else:
+                itr = itr.next
+        return head
+            
+        
