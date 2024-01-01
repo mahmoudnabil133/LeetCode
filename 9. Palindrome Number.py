88a06cb9a1cc2966948da6829a1fec05e405b7ca
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-02 01:35:06 +0200

    palindrome

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -1,10 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        xstr = str(x)
-        y = xstr
-        y = ''.join(reversed(y))
-        if (y == xstr):
+        if (x < 0):
+           return False
+        res = 0
+        num = x
+        while x > 0:
+            y = x % 10
+            x-=y
+            x /= 10
+            res = res*10 + y
+        res = int(res)
+        if (res - num == 0):
             return True
-        return False
-
         
+        return False
