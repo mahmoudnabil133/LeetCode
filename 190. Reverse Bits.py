ca922ce2e85f7a2c5ac990304b629aefa1f02aab
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-25 20:47:29 +0200

    intBinRevInt

--- 190. Reverse Bits.py
+++ 190. Reverse Bits.py
@@ -9,6 +9,11 @@ class Solution:
         mul = 1
         Bin = 0
         dig = 0
+        """
+        covert int to bin dig var to get sum of left zeroz
+        now it doesnt have meaning but in reversed bin it will
+        be important 
+        """
         while n:
             dig +=1
             rem = n % 2
@@ -17,21 +22,26 @@ class Solution:
             n //= 2
         loop = 32 - dig
         mul2 = 1
+        "delete right zeroz as it will be ignored in reversed bin"
         while Bin:
             top = Bin % 10
             if top == 1:
                 break
             Bin //= 10
+        "rev a Bin and get rev_bin"
         rev_bin = 0
         while Bin:
             top = Bin % 10    
             rev_bin = ((rev_bin * mul2) + top)
             mul2 = 10
             Bin //= 10
-        mul3 = 1
+        "add zeros to reversed bin"
         for i in range(loop):
             rev_bin *= 10
         rev_int = 0
+        
+        "finaly convert a rev_bin to int and optain rev_int"
+        mul3 = 1
         while rev_bin:
             top = rev_bin % 10
             rev_int += (top * mul3)
