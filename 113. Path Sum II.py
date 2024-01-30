8919e4ece2de7397436572c2606b3a8c514b9158
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-30 17:25:57 +0200

    path

--- 113. Path Sum II.py
+++ 113. Path Sum II.py
@@ -10,19 +10,16 @@ class Solution:
             return []
         if root.val == targetSum:
             return [[root.val]]
-        else:
-            path1 = self.pathSum(root.left, targetSum - root.val)
-            path2 = self.pathSum(root.right, targetSum - root.val)
-            ret = []
-            if path1:
-                for path in path1:
-                    path.insert(0, root.val)
-                    ret.append(path)
-                return ret
-            if path2:
-                for path in path2:
-                    path.insert(0, root.val)
-                    ret.append(path)
-                return ret
+        path1 = self.pathSum(root.left, targetSum - root.val)
+        path2 = self.pathSum(root.right, targetSum - root.val)
+        ret = []
+        if path1:
+            for path in path1:
+                path.insert(0, root.val)
+                ret.append(path)
+        if path2:
+            for path in path2:
+                path.insert(0, root.val)
+                ret.append(path)
             
         return ret
