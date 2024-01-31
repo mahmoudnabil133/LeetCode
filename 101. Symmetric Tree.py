82bbcc969924693c34e0608e8181feff21a8dc6f
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-31 16:00:09 +0200

    check if Tree is symmetreic or not

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return (self.sameTree(root.left, root.right))
+    
+    def sameTree(self,l, r):
+        if not l and not r:
+            return True
+        elif not l or not r:
+            return False
+        if (l.val != r.val):
+            return False
+        if (l.val == r.val and not r.left and not r.right and not l.left and not l.right):
+            return True
+        if (self.sameTree(l.left, r.right) and self.sameTree(l.right, r.left)):
+            return True
+        return False
