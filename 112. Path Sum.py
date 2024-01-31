1639c73225693787fcdf4b247fe6493e869ccb66
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-31 17:58:35 +0200

    pathSum

---
+++ 112. Path Sum.py
@@ -0,0 +1,19 @@
+"""
+Check if tree has a path with sum = targetSum
+solved by recursion
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        if (root.val == targetSum and not root.left and not root.right):
+            return True
+        fndLeft = self.hasPathSum(root.left, targetSum - root.val)
+        fndRight = self.hasPathSum(root.right, targetSum - root.val)
+        return (fndLeft + fndRight)
