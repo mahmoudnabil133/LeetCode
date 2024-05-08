b58111b1dca0c052ef93e7b5af2410910de4e7c0
mahmoudnabil133 <hodanabil144@gmail.com>
2024-05-08 15:26:32 +0300

    array pivot

---
+++ 724. Find Pivot Index.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        sumLeft = [0] * n
+        sumRight = [0] * n
+
+        totLeft , totRight = 0, sum(nums)
+
+        for i in range(len(nums)):
+            sumLeft[i] = totLeft
+            totLeft += nums[i]
+            totRight -= nums[i]
+            sumRight[i] = totRight
+            if sumLeft[i] == sumRight[i]:
+                return i
+        return -1
