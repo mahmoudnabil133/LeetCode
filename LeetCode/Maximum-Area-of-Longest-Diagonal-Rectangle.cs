e1bd8c278c836ca2da5953000487ab9ff34bd05d
mahmoudnabil133 <125285440+mahmoudnabil133@users.noreply.github.com>
2025-08-26 22:07:33 +0300

    [3000] [Time Beats: 0.00%] [Memory Beats: 9.52%] - LeetPush

---
+++ LeetCode/Maximum-Area-of-Longest-Diagonal-Rectangle.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int AreaOfMaxDiagonal(int[][] dimensions) {
+        double maxDiagonal = 0, curDiagonal = 0;
+        int maxArea = 0, curArea= 0;
+        for(int i=0; i < dimensions.Length; i++){
+            curDiagonal = Math.Sqrt((dimensions[i][0] * dimensions[i][0]) +(dimensions[i][1] * dimensions[i][1]) );
+            curArea = dimensions[i][0] * dimensions[i][1];
+            if(curDiagonal > maxDiagonal){
+                maxDiagonal = curDiagonal;
+                maxArea =  dimensions[i][0] * dimensions[i][1];
+            } else if (curDiagonal == maxDiagonal){
+                maxArea = (int) Math.Max(maxArea, curArea);
+            }
+            Console.WriteLine($"curDiag = {curDiagonal}, maxDiagonal = {maxDiagonal}, curArea = {curArea},  maxArea = {maxArea}");
+        }
+        return maxArea;
+    }
+}
