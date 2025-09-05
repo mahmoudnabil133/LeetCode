a2323c47a68dbd552e9a79a658bb99f571ae65c7
mahmoudnabil133 <125285440+mahmoudnabil133@users.noreply.github.com>
2025-09-05 23:17:32 +0300

    [1431] [Time Beats: 29.32%] [Memory Beats: 62.97%] - LeetPush

---
+++ LeetCode/Kids-With-the-Greatest-Number-of-Candies.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        int max = candies.Max();
+        return candies.Select(x=> x+extraCandies >= max).ToList();
+    }
+}
