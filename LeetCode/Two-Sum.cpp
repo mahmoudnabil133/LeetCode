ae37d296ddafd38435c3a00b95913196f98cbcdb
mahmoudnabil133 <125285440+mahmoudnabil133@users.noreply.github.com>
2026-01-21 18:32:13 +0200

    [1] [Time Beats: 67.71%] [Memory Beats: 13.09%] - LeetPush

--- LeetCode/Two-Sum.cpp
+++ LeetCode/Two-Sum.cpp
@@ -1,18 +1,13 @@
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4        vector<int> out;
-5        unordered_map<int, int> mp;
-6        int temp;
-7        for(int i =0; i<nums.size(); i++){
-8            temp = target - nums[i];
-9            if(mp.count(temp)){
-10                out.push_back(mp[temp]);
-11                out.push_back(i);
-12                return out;
-13            }
-14            mp[nums[i]] = i;
-15        }
-16        return out;
-17    }
-18};
+4        map<int, int> mp;
+5        int secNum = 0;
+6        for(int i=0; i<nums.size(); i++){
+7            secNum = target - nums[i];
+8            if(mp.count(secNum)) return {i, mp[secNum]};
+9            mp[nums[i]] = i;
+10        }
+11        return {-1, -1};
+12    }
+13};
