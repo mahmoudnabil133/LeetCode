93e22a6cdd30256532b73709484492ec2f4eb164
mahmoudnabil133 <125285440+mahmoudnabil133@users.noreply.github.com>
2026-01-21 23:23:13 +0200

    [49] [Time Beats: 90.73%] [Memory Beats: 39.14%] - LeetPush

--- LeetCode/Group-Anagrams.cpp
+++ LeetCode/Group-Anagrams.cpp
@@ -1,23 +1,20 @@
 1class Solution {
 2public:
 3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-4        map<string, vector<string>> mp;
+4        unordered_map<string, vector<string>> mp;
 5        vector<vector<string>> res;
 6        string temp;
 7
 8        for (auto s:strs){
 9            temp = s;
 10            sort(s.begin(), s.end());
-11            if(mp.count(s)){
-12                mp[s].push_back(temp);
-13            } else{
-14                mp[s] = {temp};
-15            }
+11            if(mp.count(s)) mp[s].push_back(temp);
+12            else mp[s] = {temp};
+13        }
+14        for (auto it:mp){
+15            res.push_back(it.second);
 16        }
-17        for (auto it:mp){
-18            res.push_back(it.second);
-19        }
-20        return res;
-21        
-22    }
-23};
+17        return res;
+18        
+19    }
+20};
