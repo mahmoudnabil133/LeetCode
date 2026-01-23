9fe594da4f8bfd34bfb45aa625abb7fc2df5e57f
mahmoudnabil133 <125285440+mahmoudnabil133@users.noreply.github.com>
2026-01-23 20:15:16 +0200

    [733] [Time Beats: 100.00%] [Memory Beats: 43.56%] - LeetPush

--- LeetCode/Flood-Fill.cpp
+++ LeetCode/Flood-Fill.cpp
@@ -1,35 +1,22 @@
 1class Solution {
 2public:
-3
-4    int org_color, new_color;
-5    int rows, cols;
-6    vector<vector<int>> *img;
-7    // cell should be as original to fill ==> (*img)[i][j] != org_color
-8    void dfs(int i, int j){
-9        if(i < 0 || i>= rows || j<0 || j>= cols || (*img)[i][j] != org_color) 
-10            return;
-11
-12        (*img)[i][j] = new_color;
-13        
-14        dfs(i-1, j);
-15        dfs(i, j+1);
-16        dfs(i+1, j);
-17        dfs(i, j-1);
-18    }
-19    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-20
-21        org_color = image[sr][sc];
-22        rows = image.size();
-23        cols = image[0].size();
-24        new_color = color;
-25        img = &image;
-26
-27
-28        // if new color same as original so no need to flood fill
-29        if(org_color == new_color){
-30            return image;
-31        }
-32        dfs(sr, sc);
-33        return image;
-34    }
-35};
+3    int oldColor;
+4    vector<vector<int>> img;
+5    int newColor;
+6    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+7        img = image;
+8        newColor = color;
+9        oldColor = image[sr][sc];
+10        if(oldColor == color) return image;
+11        dfs(sr, sc);
+12        return img;
+13    }
+14    void dfs(int r, int c){
+15        if(r<0 || r>= img.size() || c<0 || c>=img[0].size() || img[r][c] != oldColor) return;
+16        img[r][c] = newColor;
+17        dfs(r-1, c);
+18        dfs(r+1, c);
+19        dfs(r, c-1);
+20        dfs(r, c+1);        
+21    }
+22};
